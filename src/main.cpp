63c87449f269f179accbf987b338cd456d3d3868
SlimeCodex <ale.nicolini.p@gmail.com>
2023-11-18 16:44:59 -0300

    updated example ssc

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,38 @@
-// simple_stream example, this example code shows how to create and publish data to two simple monitors
+// simple_stream: this example code shows how to create and publish data to two simple monitors
 
 #include <SmartSyncEvent.h>
 #include <SkyStreamConsole.h>
+
+// Deps for the random generator
 #include <cstdlib>
 #include <ctime>
 
+// Initialize consoles and handler
 SkyStreamConsole monitor_core("Core Monitor");
 SkyStreamConsole monitor_wifi("WiFi Monitor");
 SSCHandler ssc_handler;
 
+// 10 Digits random number generator for the example
+unsigned long long generateRandom10DigitNumber() {
+    unsigned long long number = rand() % 9 + 1;
+    for (int i = 1; i < 10; ++i) {
+        number = number * 10 + (rand() % 10);
+    }
+    return number;
+}
+
 void setup() {
+	// Generate consoles
 	ssc_handler.begin();
 	ssc_handler.add(monitor_core);
 	ssc_handler.add(monitor_wifi);
 	ssc_handler.start();
-
-	// Seed the random number generator
-	srand(time(NULL));
 }
 
 void loop() {
+	// Print to consoles time and random data
 	if (SYNC_EVENT(100)) {
-		char randomCharCore = 'C' + rand() % 26; // This will generate a random uppercase letter
-		monitor_core.printf("%lu Core Monitor Data: %c\n", millis(), randomCharCore);
-
-		char randomCharWifi = 'W' + rand() % 26; // This will generate a random uppercase letter
-		monitor_wifi.printf("%lu WiFi Monitor Data: %c\n", millis(), randomCharWifi);
+		monitor_core.printf("%lu Core Monitor Data: %llu\n", millis(), generateRandom10DigitNumber());
+		monitor_wifi.printf("%lu WiFi Monitor Data: %llu\n", millis(), generateRandom10DigitNumber());
 	}
 }
